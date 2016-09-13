affbbdee1c9a19bcf2076e70325a28372c99ee42
anilkunchalaece <anilkunchalaece@gmail.com>
2016-09-13 18:14:38 +0530

    Added code to read moisture sensor value with Analog Input It Receives 3.3v max value so no need of voltage devider

--- dthSample.lua
+++ dthSample.lua
@@ -1,10 +1,14 @@
 --Example from NodeMcu Docs
 -- After Saving this file in NodeMcu using Esplorer run the file using dofile('dthSample.lua')
+-- https://nodemcu.readthedocs.io/en/master/en/modules/dht/
+--http://www.barissanli.com/electronics/nodemcu.php
 pin = 5
 status, temp, humi, temp_dec, humi_dec = dht.read(pin)
 if status == dht.OK then
     -- Float firmware using this example
     print("DHT Temperature:"..temp..";".."Humidity:"..humi)
+	--read the Moisture sensor Value
+	print (adc.read(0))
 
 elseif status == dht.ERROR_CHECKSUM then
     print( "DHT Checksum error." )
