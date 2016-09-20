92ec8bb0249a9299da43d53e13a85c8025407daa
Kunchala Anil <anilkunchalaece@gmail.com>
2016-09-20 20:02:36 +0530

    Echoing the post Request Data

---
+++ sensorData.php
@@ -0,0 +1,16 @@
+<?php
+echo "Hello Anil";
+
+echo 'moisture Value';
+echo $_POST['mois'];
+
+echo 'temp Value';
+echo $_POST['temp'];
+
+echo 'humidity';
+echo $_POST['humi'];
+
+echo 'ldr Value';
+echo $_POST['ldr']
+
+?>
