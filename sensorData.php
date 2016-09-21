54e3273a640d9f00785a613b176fc55380491011
Kunchala Anil <anilkunchalaece@gmail.com>
2016-09-21 16:41:47 +0530

    database addition is Working

--- sensorData.php
+++ sensorData.php
@@ -1,16 +1,49 @@
+//Author : Kunchala Anil
+//Email : anilkunchalaece@gmail.com
+//php code to Get the Data from Post Request send from Node Mcu and Save it in the Database
+//site : padma1ver2.16mb.com
+
 <?php
 echo "Hello Anil";
 
 echo 'moisture Value';
-echo $_POST['mois'];
+$mois =  $_POST['mois'];
 
 echo 'temp Value';
-echo $_POST['temp'];
+$temp = $_POST['temp'];
 
 echo 'humidity';
-echo $_POST['humi'];
+$humi =  $_POST['humi'];
 
 echo 'ldr Value';
-echo $_POST['ldr']
+$ldr = $_POST['ldr'];
 
+echo $mois;
+echo $temp;
+echo $humi;
+echo $ldr;
+
+
+$serverName = "mysql.hostinger.in";
+$userName = "u576780510_anil";
+$password = "123456";
+$db = "u576780510_padma";
+
+//Create Connection
+$conn = new mysqli($serverName,$userName,$password,$db);
+if($conn ->connect_error){
+	die("Connection Failed: ".$conn->connect_error);
+	}
+echo "Connected successfully";
+
+$sql = "INSERT INTO sensorData(time, ldr, humi,temp,mois)
+VALUES ( date('Y/m/d H:i:s'),$ldr,$humi,$temp,$mois)";
+
+if (mysqli_query($conn, $sql)) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+
+mysqli_close($conn);
 ?>
