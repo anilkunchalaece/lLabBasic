309fef26a4124d818fd1a61f0b53bae05a69c0ca
Kunchala Anil <anilkunchalaece@gmail.com>
2016-09-21 16:51:23 +0530

    Update padma.php

--- padma.php
+++ padma.php
@@ -1,23 +1,23 @@
+<!DOCTYPE html>
+
+<!DOCTYPE html>
+<!--
+Author : Kunchala Anil,
+Email : anilkunchalaece@gmail.com
+Date : 21 October 2016
+
+This code receives the Data From MySQl and Displays it as Webpage as a Table
+-->
+
+<html>
+<head>
+<title> Sensor Data </title>
+</head>
+
+<body>
+<h2> Sensor Data From padma1Ver2</h2>
+
 <?php
-echo "Hello Anil";
-
-echo 'moisture Value';
-$mois =  $_POST['mois'];
-
-echo 'temp Value';
-$temp = $_POST['temp'];
-
-echo 'humidity';
-$humi =  $_POST['humi'];
-
-echo 'ldr Value';
-$ldr = $_POST['ldr'];
-
-echo $mois;
-echo $temp;
-echo $humi;
-echo $ldr;
-
 
 $serverName = "mysql.hostinger.in";
 $userName = "u576780510_anil";
@@ -30,15 +30,38 @@ if($conn ->connect_error){
 	die("Connection Failed: ".$conn->connect_error);
 	}
 echo "Connected successfully";
+$retval = mysqli_query($conn,"SELECT * FROM sensorData");
 
-$sql = "INSERT INTO sensorData(time, ldr, humi,temp,mois)
-VALUES ( date('Y/m/d H:i:s'),$ldr,$humi,$temp,$mois)";
-
-if (mysqli_query($conn, $sql)) {
-    echo "New record created successfully";
-} else {
-    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+if(!$retval){
+	die('Could not get Data : '.mysqli_error());
 }
+echo "<table style='border : 1px solid black'>";
 
-mysqli_close($conn);
+echo "<tr>
+	<td style='border : 1px solid black'>Time Stamp</td>
+	<td style='border : 1px solid black'>Moisture</td>
+	<td style='border : 1px solid black'>Humidity</td>
+	<td style='border : 1px solid black'>Temperature</td>
+        <td style='border : 1px solid black'>Light Intensity</td>
+</tr>";
+
+while($row = mysqli_fetch_assoc($retval)){
+ echo "<tr >
+	<td style='border : 1px solid black'>{$row['time']}</td>
+	<td style='border : 1px solid black'>{$row['mois']}</td>
+	<td style='border : 1px solid black'>{$row['humi']}</td>
+	<td style='border : 1px solid black'>{$row['temp']}</td>
+        <td style='border : 1px solid black'>{$row['ldr']}</td>
+	</tr>";
+}
+echo "</table>";
+
+echo "Fetched Data Sucessfully";
 ?>
+
+<footer>
+  <p>Done by: Kunchala Anil</p>
+  <p>Email : anilkunchalaece@gmail.com</p>
+</footer>
+</body>
+</html>	
