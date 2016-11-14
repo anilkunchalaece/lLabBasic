530f8f0e4f6559bf9cd43bab17f2b89e3659600c
Kunchala Anil <anilkunchalaece@gmail.com>
2016-11-14 20:07:40 +0530

    Update postData.lua

--- postData.lua
+++ postData.lua
@@ -6,15 +6,16 @@ Date : Nov 14 2016
 
 dofile("readSensorData.lua") --run this file to read the sensor values
 
-data = 'device_id='..node.chipid()..'&data_time='.. 40 .. '&soil_moisture='.. moistureValue ..'&temperature='.. tempValue ..'&humidity='.. humiValue ..'&luminance='..ldrValue
-print(data)
+--data = 'device_id='..node.chipid()..'&data_time='.. 40 .. '&soil_moisture='.. moistureValue ..'&temperature='.. tempValue ..'&humidity='.. humiValue ..'&luminance='..ldrValue
+pData = '&mois='.. moistureValue ..'&temp='.. tempValue ..'&humi='.. humiValue ..'&ldr='..ldrValue --for padma1ver2.16mb.com
+print(pData)
 
-http.request("http://pamda1ver2.16mb.com/sensorData.php", "POST", "Content-Type: application/x-www-form-urlencoded\r\n",data,
+http.request("http://pamda1ver2.16mb.com/sensorData.php", "POST", "Content-Type: application/x-www-form-urlencoded\r\n",pData,
 --http.request("http://ic.ksrmced.in/addData.php", "POST", "Content-Type: application/x-www-form-urlencoded\r\n",data,
-  function(code, data)
+  function(code, rData)
     if (code < 0) then
       print("HTTP request failed")
     else
-      print(code, data)
+      print(code, rData)
     end
   end)
